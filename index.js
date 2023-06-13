fcf62f4b3d6afd7cacf1e4424c4237d08be0adce
Ralph Shui <rskc.2112@gmail.com>
2023-06-13 17:02:10 -0400

    completing first class functions lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = names => [names[0], names[1]];
+
+const returnLastTwoDrivers = names => [names[names.length-2], names[names.length-1]];
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(multiplier) {
+    return function(fare) {
+        return fare * multiplier;
+    }
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = (names, fn) => fn(names);
